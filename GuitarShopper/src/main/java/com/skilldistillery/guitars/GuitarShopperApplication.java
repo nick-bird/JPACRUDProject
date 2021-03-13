2988d837c36fbce4b92197a4b7a63be132b7e708
Nick Bird <bird.nick.95@gmail.com>
2021-03-12 17:30:21 -0700

    Stub Boot/MVC project, link JPA dependency

---
+++ GuitarShopper/src/main/java/com/skilldistillery/guitars/GuitarShopperApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.guitars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarShopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarShopperApplication.class, args);
+	}
+
+}
---
+++ GuitarShopper/src/test/java/com/skilldistillery/guitars/GuitarShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.guitars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarShopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
